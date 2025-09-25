eb9685e1a021dc79d1822a829aaf631f3c41258f
Hadi-crypto2 <f44@hotmail.it>
2025-09-25 13:57:52 +0300

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/health", (_, res) => {
+  res.json({ ok: true });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
